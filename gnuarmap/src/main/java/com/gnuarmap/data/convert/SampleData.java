0bac18af6e32230a170c2ade84334fbbdb1889b6
ji pyeong kang <jpg3927@gmail.com>
2017-11-15 10:28:11 +0900

    샘플 데이터 추가

--- gnuarmap/src/main/java/com/gnuarmap/data/convert/SampleData.java
+++ gnuarmap/src/main/java/com/gnuarmap/data/convert/SampleData.java
@@ -16,6 +16,11 @@ public class SampleData {
         data.addItem("상아리더스타운","http://m.naver.com",35.263049, 128.640294, "PINK",25);
         data.addItem("금강목련아파트","http://m.naver.com",35.262593,128.639853, "BLACK",25);
         data.addItem("CU편의점","http://m.naver.com",35.263136,128.639374, "GREEN",25);
+
+        data.addItem("도서관 매점","http://m.naver.com",35.1537364,128.1015611, "GREEN",13);
+        data.addItem("야외공연장","http://m.naver.com",35.1521609,128.1029144, "GREEN",13);
+        data.addItem("경영대학","http://m.naver.com",35.1538200,128.099861, "GREEN",13);
+        data.addItem("학생회관","http://m.naver.com",35.1538205,128.0973951,"GREEN",12);
        // 샘플 데이터, 위 방식대로 학교의 건물들을 모두 등록한다.
     }
 }
