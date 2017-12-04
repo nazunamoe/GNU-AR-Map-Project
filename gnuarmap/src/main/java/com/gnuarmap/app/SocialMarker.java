81e605c904ca56046312dd283e4d9ca06b0a1520
ji pyeong kang <jpg3927@gmail.com>
2017-12-04 19:55:16 +0900

    너무 가까운 마커는 표시하지 않는다

--- gnuarmap/src/main/java/com/gnuarmap/app/SocialMarker.java
+++ gnuarmap/src/main/java/com/gnuarmap/app/SocialMarker.java
@@ -62,27 +62,29 @@ public class SocialMarker extends Marker {
 
     @Override
     public void draw(PaintScreen dw) {
+        if(this.distance<30){
+            // 텍스트 블록을 그린다
+            drawTextBlock(dw);
 
-        // 텍스트 블록을 그린다
-        drawTextBlock(dw);
+            // 보여지는 상황이라면
+            if (isVisible) {
+                float maxHeight = Math.round(dw.getHeight() / 10f) + 1;	// 최대 높이 계산
+                // 데이터 소스의 비트맵 파일을 읽어온다
 
-        // 보여지는 상황이라면
-        if (isVisible) {
-            float maxHeight = Math.round(dw.getHeight() / 10f) + 1;	// 최대 높이 계산
-            // 데이터 소스의 비트맵 파일을 읽어온다
+                Bitmap bitmap = Dataclass.getBitmap(getFlag);
+                // 비트맵 파일이 읽혔다면 적절한 위치에 출력
+                if(bitmap!=null) {
+                    dw.paintBitmap(bitmap, cMarker.x - maxHeight/1.5f, cMarker.y - maxHeight/0.6f);
+                }
+                else {	// 비트맵 파일을 갖지 않는 마커의 경우
 
-            Bitmap bitmap = Dataclass.getBitmap(getFlag);
-            // 비트맵 파일이 읽혔다면 적절한 위치에 출력
-            if(bitmap!=null) {
-                dw.paintBitmap(bitmap, cMarker.x - maxHeight/1.5f, cMarker.y - maxHeight/0.6f);
-            }
-            else {	// 비트맵 파일을 갖지 않는 마커의 경우
-
-                dw.setStrokeWidth(maxHeight / 10f);
-                dw.setFill(false);
-                dw.paintCircle(cMarker.x, cMarker.y, maxHeight / 1.5f);
+                    dw.setStrokeWidth(maxHeight / 10f);
+                    dw.setFill(false);
+                    dw.paintCircle(cMarker.x, cMarker.y, maxHeight / 1.5f);
+                }
             }
         }
+
     }
 
     public boolean fClick(float x, float y, MixContext ctx, State state) {
